454925861c92eba1b6ba3e22c9d154a204372f9f
Ayesha Habib <121867357+ayesha-habib07@users.noreply.github.com>
2025-03-18 23:42:22 +0500

    Initial commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,16 +7,7 @@ import ChatWithAttorney from '../components/ChatWithAttorney';
 import FindLawyer from '../components/FindLawyer';
 import StatsSection from '../components/StatsSection';
 import ContactSection from './ContactSection';
-// import Carousel from '../Components/Carousel'
-// import Rent from '../Components/Rent'
-// import StatsSection from '../Components/StatsSection'
-// import ExploreApartments from '../Components/ExploreApartments'
-// import LogoCarousel from '../Components/LogoCarousel'
-// import Testimonials from '../Components/Testimonials'
-// import DreamHouseForm from '../Components/DreamHouseForm'
-// import SellingOption from '../Components/SellingOption'
-// import Agents from '../Components/Agents'
-// import houseImage from "../assets/house5.jpg"; 
+
 const HomePage = () => {
     return (
         <>
@@ -29,15 +20,6 @@ const HomePage = () => {
             <FindLawyer />
             <ContactSection />
           
-            {/* <Carousel />
-            <Rent />
-            <SellingOption  isHomePage={true}/>
-            <StatsSection />
-            <ExploreApartments />
-            <Agents isHomePage={true} /> */}
-            {/* <LogoCarousel /> */}
-            {/* <Testimonials  houseImage={houseImage} heading =" What Our Clients Say" />
-            <DreamHouseForm /> */}
         </>
     )
 }
